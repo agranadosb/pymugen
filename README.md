0a93fede6edc03d5e23ec0940c109275e864079a
Alejandro Granados <alejandro@dekalabs.com>
2021-08-01 16:13:03 +0200

    fix(documentation): fix documentation readme

--- README.md
+++ README.md
@@ -1,63 +1,61 @@
-pymugen
--------
+# pymugen
 
 This module allows to manage data about genomic sequences, specificaly getting sequences from a FASTA file.
 
 The module's other functionality is the ability to get sequences with a mutation from a VCF file and transform it to another representation.
 
 
-Installation
-------------
+## Installation
 
 Install pymugen with python
 
-``pip3 install pymugen``
+```bash
+  pip3 install pymugen
+```
 
-Usage/Examples
---------------
+## Usage/Examples
 
 Fasta example
 
-.. code-block:: python
+```python
+from pymugen.fasta import FastaReader
 
-  from pymugen.fasta import FastaReader
+fasta = FastaReader("fasta.fa")
 
-  fasta = FastaReader("fasta.fa")
+chromosome = fasta['chr1']
+sequence = chromosome.sequence(10, 5, 5)
 
-  chromosome = fasta['chr1']
-  sequence = chromosome.sequence(10, 5, 5)
+sequence[1]
+>>> A
 
-  sequence[1]
-  >>> A
+sequence[:]
+>>> CATGCATGCAT
 
-  sequence[:]
-  >>> CATGCATGCAT
+str(sequence)
+>>> C-A-T-G-C,A,T-G-C-A-T
 
-  str(sequence)
-  >>> C-A-T-G-C,A,T-G-C-A-T
-
-  chromosome[5: 16]
-  >>> CATGCATGCAT
-
+chromosome[5: 16]
+>>> CATGCATGCAT
+```
 
 VCF example
 
-.. code-block:: python
+```python
+from pymugen.transformers import ExtendedTransformer
+from pymugen.fasta import FastaReader
 
-  from pymugen.transformers import ExtendedTransformer
-  from pymugen.fasta import FastaReader
+vcf = ExtendedTransformer("vcf.vcf", "fasta.fa")
 
-  vcf = ExtendedTransformer("vcf.vcf", "fasta.fa")
+fasta = FastaReader("fasta.fa")
 
-  fasta = FastaReader("fasta.fa")
+chromosome = fasta['chr1']
+sequence = chromosome.sequence(10, 5, 5)
 
-  chromosome = fasta['chr1']
-  sequence = chromosome.sequence(10, 5, 5)
+mutated = vcf.method(sequence, "CGT")
 
-  mutated = vcf.method(sequence, "CGT")
+str(mutated)
+>>> 'A|w-q-r-e-w,s-d-f,v-c-x-z-v'
 
-  str(mutated)
-  >>> 'A|w-q-r-e-w,s-d-f,v-c-x-z-v'
-
-  mutated[:]
-  >>> 'wqrewsdfvcxzv'
+mutated[:]
+>>> 'wqrewsdfvcxzv'
+```
--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-from os import path
-
 from setuptools import find_packages, setup
-
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-  long_description = f.read()
 
 setup(
   name = 'pymugen',
   packages = find_packages(),
-  version = '1.2',
+  version = '1.2.1',
   license='MIT',
   description = 'Management about FASTA and VCF files.',
-  long_description=long_description,
   long_description_content_type='text/markdown',
   author = 'Alejandro Granados',
   author_email = 'agranadosb55199@outlook.com',
@@ -30,4 +23,70 @@ setup(
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.8',
   ],
+  long_description="""
+  pymugen
+  -------
+
+  This module allows to manage data about genomic sequences, specificaly getting sequences from a FASTA file.
+
+  The module's other functionality is the ability to get sequences with a mutation from a VCF file and transform it to another representation.
+
+
+  Installation
+  ------------
+
+  Install pymugen with python
+
+  ``pip3 install pymugen``
+
+  Usage/Examples
+  --------------
+
+  Fasta example
+
+  ::
+
+    from pymugen.fasta import FastaReader
+
+    fasta = FastaReader("fasta.fa")
+
+    chromosome = fasta['chr1']
+    sequence = chromosome.sequence(10, 5, 5)
+
+    sequence[1]
+    # A
+
+    sequence[:]
+    # CATGCATGCAT
+
+    str(sequence)
+    # C-A-T-G-C,A,T-G-C-A-T
+
+    chromosome[5: 16]
+    # CATGCATGCAT
+
+
+  VCF example
+
+  ::
+
+    from pymugen.transformers import ExtendedTransformer
+    from pymugen.fasta import FastaReader
+
+    vcf = ExtendedTransformer("vcf.vcf", "fasta.fa")
+
+    fasta = FastaReader("fasta.fa")
+
+    chromosome = fasta['chr1']
+    sequence = chromosome.sequence(10, 5, 5)
+
+    mutated = vcf.method(sequence, "CGT")
+
+    str(mutated)
+    # 'A|w-q-r-e-w,s-d-f,v-c-x-z-v'
+
+    mutated[:]
+    # 'wqrewsdfvcxzv'
+
+  """,
 )
